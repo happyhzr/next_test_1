4ad1e3ddae50af99568210aaabfc27cc5fd480cc
happyhzr <insisthzr@163.com>
2025-07-18 17:17:38 +0800

    add themes and fonts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import { SignOut } from "@/components/sign-out";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl">
+      <h1 className="text-2xl text-red-500">
         Hello world!
       </h1>
     </div>
