ebd7d48bd68461702573fc8bec650d7fa028e163
happyhzr <insisthzr@163.com>
2025-07-21 16:57:17 +0800

    add shadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="heading">
         Hello world!
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
