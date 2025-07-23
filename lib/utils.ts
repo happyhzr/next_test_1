b4acfbd20c657016a6870a01377cb26e1b0d3267
happyhzr <insisthzr@163.com>
2025-07-23 17:18:50 +0800

    add a card

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  };
+  return new Date(date).toLocaleDateString('en-US', options);
+}
