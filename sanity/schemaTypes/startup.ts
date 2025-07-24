9f04742189232774a724214b40ee92f2f574c51d
happyhzr <insisthzr@163.com>
2025-07-24 17:30:51 +0800

    add schema

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,47 @@
+import { defineField, defineType } from 'sanity'
+
+export const startup = defineType({
+    name: 'startup',
+    title: 'Startup',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+            },
+        }),
+        defineField({
+            name: 'title',
+            type: 'string',
+        }),
+        defineField({
+            name: 'author',
+            type: 'reference',
+            to: { type: 'author' },
+        }),
+        defineField({
+            name: 'views',
+            type: 'number',
+        }),
+        defineField({
+            name: 'description',
+            type: 'text',
+        }),
+        defineField({
+            name: 'category',
+            type: 'string',
+            validation: (Rule) => Rule.required().min(3).max(50).error('Category must be between 3 and 50 characters long'),
+        }),
+        defineField({
+            name: 'image',
+            type: 'url',
+            validation: (Rule) => Rule.required().error('Image must be a valid URL'),
+        }),
+        defineField({
+            name: 'pitch',
+            type: 'markdown',
+        }),
+    ],
+})
