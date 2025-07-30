795a523a8b500bd33f20c566e66a1c37cb6315c8
happyhzr <insisthzr@163.com>
2025-07-30 18:09:06 +0800

    update next to support ppr

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -8,3 +8,12 @@ export const STARTUP_QUERY = defineQuery(`
     },
     views, description, category, image
 }`)
+
+export const STARTUP_BY_ID_QUERY = defineQuery(`
+    *[_type=='startup'&&_id==$id][0]{
+  _id,title,slug,_createdAt,
+    author->{
+      _id,id,name,username,slug,image,bio
+    },
+    views,description,category,image,pitch
+}`)
